db341de5a2e5ef1b496dec68fb4543fb373bd5c1
Yurii Denkovych <yd2004@ukr.net>
2022-05-17 14:17:07 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,33 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-const instance = basicLightbox.create(`
-    <div class="modal">
-        <p>
-            Your first lightbox with just a few lines of code.
-            Yes, it's really that simple.
-        </p>
-    </div>
-`)
-instance.close()
+    const instance = basicLightbox.create(`
+        <div class="modal">
+            <p>
+                Your first lightbox with just a few lines of code.
+                Yes, it's really that simple.
+            </p>
+        </div>`)
+    instance.close();
 
 const listGallery = document.querySelector('.gallery');
 
-listGallery.addEventListener('click', onClickImage)
-       
+listGallery.addEventListener('click', onClickImage);
+    
 
-const markup = galleryItems.map((image)=>
-`<div class="gallery__item">
-  <a class="gallery__link" href="${image.original}">
-    <img
-      class="gallery__image"
-      src="${image.preview}"
-      data-source="${image.original}"
-      alt="${image.description}"
-    />
-  </a>
-</div>`).join('');
-listGallery.insertAdjacentHTML( 'beforeend', markup);
-const img = document.querySelector('.gallery__image');
+    const markup = galleryItems.map((image)=>
+        `<div class="gallery__item">
+        <a class="gallery__link" href="${image.original}">
+            <img
+            class="gallery__image"
+            src="${image.preview}"
+            data-source="${image.original}"
+            alt="${image.description}"
+            />
+        </a>
+        </div>`).join('');
+    listGallery.insertAdjacentHTML( 'beforeend', markup);
+    const img = document.querySelector('.gallery__image');
     console.log(img);
 
 
@@ -36,14 +35,24 @@ function onClickImage(event){
    if(event.target.nodeName !== "IMG"){
        return; 
     }
-    
-    const srcImages = img.getAttribute('src');
-    
+    console.log(event.target)
+    const srcImages = event.target.dataset.source;
     const instance = basicLightbox.create(
     `<img src="${srcImages}">`
-)
-    instance.show(  );
-    console.log(event.target);
-
+    )
+    instance.show(  ); }
     
-}
+       
+listGallery.addEventListener('keydown',modalOpen);
+    
+    
+function modalOpen(){
+        const qwe = document.querySelector(".basicLightbox--img");
+    if(!instance.show(  )){
+      return;  
+    }
+     
+     //console.log(qwe);  
+    
+    !instance.show();
+} 
