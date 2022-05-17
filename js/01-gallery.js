20c4e3a9cb121caa33e1cabbe587445bf7c0d724
Yurii Denkovych <95170165+Denkovych@users.noreply.github.com>
2022-05-17 10:36:53 +0300

    Add files via upload

---
+++ js/01-gallery.js
@@ -0,0 +1,49 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const instance = basicLightbox.create(`
+    <div class="modal">
+        <p>
+            Your first lightbox with just a few lines of code.
+            Yes, it's really that simple.
+        </p>
+    </div>
+`)
+instance.close()
+
+const listGallery = document.querySelector('.gallery');
+
+listGallery.addEventListener('click', onClickImage)
+       
+
+const markup = galleryItems.map((image)=>
+`<div class="gallery__item">
+  <a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>
+</div>`).join('');
+listGallery.insertAdjacentHTML( 'beforeend', markup);
+const img = document.querySelector('.gallery__image');
+    console.log(img);
+
+
+function onClickImage(event){
+    event.preventDefault();
+   if(event.target.nodeName !== "IMG"){
+       return; 
+    }
+    
+    const srcImages = img.getAttribute('src');
+    
+    const instance = basicLightbox.create(
+    `<img src="${srcImages}">`
+)
+    instance.show(  );
+    console.log(event.target);
+
+    
+}
